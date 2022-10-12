2f96deb249a0ccf7ffccec4e56efa7f67531bea6
bochka123 <sasha2003tkach001100@gmail.com>
2022-10-12 17:52:58 +0300

    Completing Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
-namespace Lab1
+/*
+Oleksndr Tkach
+number of the score book - 328
+C2 - 0
+C3 - 1 
+C5 - 3
+C7 - 6
+*/
+namespace OOP
 {
-    internal class Program
+    internal class Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int C3 = 1;
+            double s = 0, a = 2, b = 3, n = 9, m = 10;
+            for (double i = a; i <= n; i++)
+            {
+                for (double j = b; j <= m; j++)
+                {
+                    s += (i + j) / (i + C3);
+                }
+            }
+            Console.WriteLine(s);
         }
     }
 }
