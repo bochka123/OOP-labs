39fdde250fff0c7ab3b08869f50cdc1469d082de
bochka123 <sasha2003tkach001100@gmail.com>
2022-10-12 23:41:19 +0300

    Lab2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -12,7 +12,34 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(328%7);
+            float[,] A = {
+                {4.3f, 9.6f, 10.0f},
+                {2.2f, 2.4f, 1.0f},
+                {2.6f, 9.8f, 2.6f}};
+            float[,] B = {
+                {5.3f, 8.2f, 15.3f},
+                {3.2f, 1.5f, 2.4f},
+                {2.7f, 11.7f, 13.5f}};
+            float[,] C = new float[A.GetLength(0),A.GetLength(1)];
+            for (int i = 0; i < C.GetLength(0); i++)
+            {
+                for (int j = 0; j < C.GetLength(1); j++)
+                {
+                    C[i,j] = (int)A[i,j] ^ (int)B[i,j];
+                    Console.Write($"{C[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < C.GetLength(0); i++)
+            {
+                float sum = 0;
+                for (int j = 0; j < C.GetLength(1); j++)
+                {
+                    sum += C[j, i];
+                }
+                float avg = sum / 3;
+                Console.WriteLine($"Середнє значення {i + 1}-ого стовпця:\t {avg}");
+            }
         }
     }
 }
