5c0781d221a04e1ae8816bc3460ea1cf550e4087
bochka123 <sasha2003tkach001100@gmail.com>
2022-10-12 18:21:36 +0300

    Editing lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 /*
-Oleksndr Tkach
-number of the score book - 328
-C2 - 0
-C3 - 1 
-C5 - 3
-C7 - 6
-*/
+ * Oleksndr Tkach
+ * number of the score book - 328
+ * C2 - 0
+ * C3 - 1 
+ * C5 - 3
+ * C7 - 6
+ */
 namespace OOP
 {
     internal class Lab1
@@ -14,7 +14,32 @@ namespace OOP
         static void Main(string[] args)
         {
             int C3 = 1;
-            double s = 0, a = 2, b = 3, n = 9, m = 10;
+            double s = 0, temp;
+            Console.Write("Enter A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Enter N: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write("Enter M: ");
+            double m = double.Parse(Console.ReadLine());
+            if (a > n)
+            {
+                temp = a;
+                a = n;
+                n = temp;
+            }
+            if (b > m)
+            {
+                temp = b;
+                b = m;
+                m = temp;
+            }
+            if (a <= -C3 && n >= -C3)
+            {
+                Console.WriteLine("Division by 0 takes place");
+                return;
+            }
             for (double i = a; i <= n; i++)
             {
                 for (double j = b; j <= m; j++)
@@ -22,7 +47,7 @@ namespace OOP
                     s += (i + j) / (i + C3);
                 }
             }
-            Console.WriteLine(s);
+            Console.WriteLine($"Result: {s}");
         }
     }
 }
---
+++ Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*
+ * Oleksndr Tkach
+ * number of the score book - 328
+ * C5 - 3
+ * C7 - 6
+ * C11 - 9
+ */
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(328%7);
+        }
+    }
+}
